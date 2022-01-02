dbdd84e2a25b7192bd633e7cedbfdd9f1dd871a2
Arghya Bhattacharya <arghya_ab2004@yahoo.in>
2022-01-02 15:13:57 +0530

    fix searches

--- main.py
+++ main.py
@@ -22,8 +22,9 @@ def login_cred_template(*args, **kwargs):
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
+        print(request.form)
         value = request.form.get("search")
-        if value:
+        if value is not None:
             query = urllib.parse.urlencode({"search": value}, doseq=False)
             return redirect(url_for("product") + f"?{query}")
         else:
@@ -55,7 +56,7 @@ def product():
 
         if request.method == "POST":
             value = request.form.get("search")
-            if value:
+            if value is not None:
                 query = urllib.parse.urlencode({"search": value}, doseq=False)
                 return redirect(url_for("product") + f"?{query}")
             else:
